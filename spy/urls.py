1c9f8b4338b473d3495fb3388fcbb5eaf47e765c
neville <kilonzoneville@gmail.com>
2018-07-20 12:30:29 +0300

    add the urls file for the different html pages

---
+++ spy/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^image/(\d+)', views.image, name ='image'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(?P<location>.*)', views.get_location, name='location'),
+    url(r'^category/(?P<category>.*)', views.get_category, name='category')
+]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
