6a2baf2c9738b1af407ac43055a0ec36a89ff001
neville <kilonzoneville@gmail.com>
2018-07-20 12:29:36 +0300

    add the view functionality for the search page

--- spy/views.py
+++ spy/views.py
@@ -11,4 +11,22 @@ def index(request):
     return render(request, 'index.html',locals())
 def image(request,image_id):
     image = Image.objects.get(id=image_id)
-    return render (request, 'image.html', {"image":image})
+    return render (request, 'image.html', {"image":image})    return render (request, 'image.html', {"image":image})
+
+
+def search_results(request):
+
+   if 'image' in request.GET and request.GET["image"]:
+       search_term = request.GET.get("image")
+       searched_images = Image.search_by_name(search_term)
+       a = searched_images[0::4]
+       b = searched_images[1::4]
+       c = searched_images[2::4]
+       d = searched_images[3::4]
+       message = f"{search_term}"
+
+       return render(request, 'search.html',{"message":message,"searched_images": searched_images}, locals())
+
+   else:
+       message = "You haven't searched for any term"
+       return render(request, 'search.html',{"message":message})
