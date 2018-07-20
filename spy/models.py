2b5ae40a305004b735b607a994877297d2b87fce
neville <kilonzoneville@gmail.com>
2018-07-20 12:24:52 +0300

    add the category model

--- spy/models.py
+++ spy/models.py
@@ -8,4 +8,14 @@ class Location(models.Model):
         return self.name
 
     def save_location(self):
+        self.save()        self.save()
+
+
+class Categoryy(models.Model):
+    name = models.CharField(max_length = 30, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
         self.save()
